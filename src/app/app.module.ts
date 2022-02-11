a8bccf49cb4adcf19690fec74c910befc0664798
nikhilyam <nik.yamsalwar@gmail.com>
2022-02-11 13:46:58 +0530

    1 file modified, 4 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AdService } from './ad.service';
+import { HeroJobAdComponent } from './hero-job-ad.component';
+import { AdBannerComponent } from './ad-banner.component';
+import { HeroProfileComponent } from './hero-profile.component';
+import { AdDirective } from './ad.directive';
 
 @NgModule({
   imports: [BrowserModule],
